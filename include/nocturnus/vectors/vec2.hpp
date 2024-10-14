ea8b742a75128023542208cf186a455b09adb5e2
vaaako <vakofernanda@gmail.com>
2024-10-14 10:45:27 -0300

    chore: imported vector from scarablib

--- include/nocturnus/vectors/vec2.hpp
+++ include/nocturnus/vectors/vec2.hpp
@@ -1,30 +1,165 @@
 #pragma once
 
-#include <tuple>
+// from scarablib
+
+// add later
+// bool operator<(const vec2& other) const {
+// 	return std::tie(x, y) < std::tie(other.x, other.y);
+// }
+
+#include <cmath>
 #include <utility>
 
 template <typename T>
 struct vec2 {
-	T x;
-	T y;
+	T x = 0;
+	T y = 0;
 
+	// The constructors NEED to be on header file
 	vec2() = default;
+	vec2(T num) : x(num), y(num) {}
 	vec2(T x, T y) : x(x), y(y) {}
-	vec2(const vec2& other) = default;
-	vec2(vec2&& other) = default;
+	vec2(const vec2& other) = default; // Copy constructor
 
-	vec2<T>& operator+(const vec2<T>& other) const noexcept {
-		return { this->x + other.x, this->y + other.y };
+	// Move constructor
+	vec2(vec2&& other) noexcept : x(std::move(other.x)), y(std::move(other.y)) {
+		other.clear();
 	}
 
-	vec2<T>& operator+=(const vec2<T>& other) noexcept {
+
+	// Set all values to zero
+	inline void clear() noexcept {
+		this->x = 0;
+		this->y = 0;
+	}
+
+	// Set values of vector
+	inline void set_values(T x, T y) noexcept {
+		this->x = x;
+		this->y = y;
+	}
+
+	// 2D Cross product (returns a scalar)
+	constexpr T cross(const vec2<T>& other) const noexcept {
+		return this->x * other.y - this->y * other.x;
+	}
+
+	// Multiplies and sums values with another vector
+	constexpr T dot(const vec2& other) const noexcept {
+		return this->x * other.x + this->y * other.y;
+	}
+
+	// The squared magnitude of the vector
+	constexpr T length_squared() const noexcept {
+		return this->dot(*this);
+	}
+
+	// The magnitude (length) of the vector
+	constexpr T length() const noexcept {
+		return std::sqrt(this->length_squared());
+	}
+
+	// Normalizes the vector to have a magnitude of 1 (unit vector)
+	constexpr vec2 normalized() const noexcept {
+		const T length = this->length();
+		return (length > 0) ? *this / length : *this;
+	}
+
+
+	// Addition
+	constexpr vec2 operator+(const vec2<T>& other) const noexcept {
+		return vec2(this->x + other.x, this->y + other.y);
+	}
+
+	constexpr vec2 operator+(const T scalar) const noexcept {
+		return vec2(this->x + scalar, this->y + scalar);
+	}
+
+	constexpr vec2& operator+=(const vec2& other) noexcept {
 		this->x += other.x;
 		this->y += other.y;
-		return { this->x + other.x, this->y + other.y };
+		return *this;
 	}
 
+	constexpr vec2& operator+=(const T scalar) noexcept {
+		this->x += scalar;
+		this->y += scalar;
+		return *this;
+	}
+
+
+	// Subtraction
+	constexpr vec2 operator-(const vec2<T>& other) const noexcept {
+		return vec2(this->x - other.x, this->y - other.y);
+	}
+
+	constexpr vec2 operator-(const T scalar) const noexcept {
+		return vec2(this->x - scalar, this->y - scalar);
+	}
+
+	constexpr vec2& operator-=(const vec2& other) noexcept {
+		this->x -= other.x;
+		this->y -= other.y;
+		return *this;
+	}
+
+	constexpr vec2& operator-=(const T scalar) noexcept {
+		this->x -= scalar;
+		this->y -= scalar;
+		return *this;
+	}
+
+
+	// Multiplication
+	constexpr vec2 operator*(const vec2<T>& other) const noexcept {
+		return vec2(this->x * other.x, this->y * other.y);
+	}
+
+	constexpr vec2 operator*(const T scalar) const noexcept {
+		return vec2(this->x * scalar, this->y * scalar);
+	}
+
+	constexpr vec2& operator*=(const vec2& other) noexcept {
+		this->x *= other.x;
+		this->y *= other.y;
+		return *this;
+	}
+
+	constexpr vec2& operator*=(const T scalar) noexcept {
+		this->x *= scalar;
+		this->y *= scalar;
+		return *this;
+	}
+
+
+	// Division
+	constexpr vec2 operator/(const vec2<T>& other) const noexcept {
+		assert(other.x != 0 && "Division by zero in x component");
+		assert(other.y != 0 && "Division by zero in y component");
+		return vec2(this->x / other.x, this->y / other.y);
+	}
+
+	constexpr vec2 operator/(const T scalar) const noexcept {
+		assert(scalar != 0 && "Division by zero");
+		return vec2(this->x / scalar, this->y / scalar);
+	}
+
+	constexpr vec2& operator/=(const vec2& other) noexcept {
+		this->x /= other.x;
+		this->y /= other.y;
+		return *this;
+	}
+
+	constexpr vec2& operator/=(const T scalar) noexcept {
+		this->x /= scalar;
+		this->y /= scalar;
+		return *this;
+	}
+
+
+	// Equality
 	bool operator==(const vec2<T>& other) const noexcept {
-		return (this->x == other.x) && (this->y == other.y);
+		return (this->x == other.x && this->y == other.y);
 	}
 
 	bool operator!=(const vec2<T>& other) const noexcept {
@@ -32,10 +167,7 @@ struct vec2 {
 	}
 
 
-	bool operator<(const vec2& other) const {
-		return std::tie(x, y) < std::tie(other.x, other.y);
-	}
-
+	// Assigment
 	vec2<T>& operator=(const vec2<T>& other) noexcept {
 		if(this != &other) {
 			this->x = other.x;
@@ -54,8 +186,10 @@ struct vec2 {
 		if (this != &other) {
 			this->x = std::move(other.x);
 			this->y = std::move(other.y);
+			other.clear();
 		}
 		return *this;
 	}
 };
+
 
